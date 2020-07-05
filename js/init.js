8546e727dd483bd78fd2a9b2765b6420adaa36a9
GlennOnyango <glennonyango@yahoo.com>
2020-07-05 16:58:00 +0300

    This

---
+++ js/init.js
@@ -0,0 +1,31 @@
+$('document').ready(function() {
+    $('select').formSelect();
+    //   $('.modal').modal();
+    $('.dropdown-trigger').dropdown();
+    $('.sidenav').sidenav();
+    $('.pushpin').pushpin();
+    $('.collapsible').collapsible();
+    $('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true
+    });
+    $('.tooltipped').tooltip();
+    $('.tabs').tabs();
+    $('.parallax').parallax();
+    //$('.materialboxed').materialbox();
+    //$('.modal').modal();
+
+    var elems = document.querySelectorAll('.modal');
+
+    var options = {
+        opacity: 0.3,
+        onCloseEnd: function(params) {
+            $("tr").remove(".new_row");
+        },
+
+        dismissible: false
+    }
+
+    M.Modal.init(elems, options);
+
+});
