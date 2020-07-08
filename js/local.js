9e7cc5284cacf8730b4593465275a873780a0c6e
GlennOnyango <glennonyango@yahoo.com>
2020-07-08 18:01:39 +0300

    This

--- js/local.js
+++ js/local.js
@@ -5,15 +5,18 @@ mytab.forEach(element => {
 
     element.addEventListener("click", () => {
 
-        console.log()
-        if (element.getAttribute("href") == "#getsupport") {
+        console.log(element.className);
+        var x = document.getElementsByClassName("" + element.className).parentNode;
+        console.log(x);
+        /*if (element.getAttribute("href") == "#getsupport") {
             // nav_con.classList.add("myfixednavheight");
             nav_con.classList.remove("navbar-fixed");
+            console.log(nav_con.className);
 
         } else {
             nav_con.classList.add("navbar-fixed");
 
-        }
+        }*/
 
     });
 });
